Havoc-OS/android_packages_apps_HavocSettings

Use new SeekBar

src/com/havoc/settings/fragments/Buttons.java
@@ -36,7 +36,7 @@ import com.android.internal.util.hwkeys.ActionConstants;
 import com.android.internal.util.hwkeys.ActionUtils;
 
 import com.havoc.settings.preferences.ActionFragment;
-import com.havoc.settings.preferences.CustomSeekBarPreference;
+import com.havoc.settings.preferences.SystemSettingSeekBarPreference;
 
 public class Buttons extends ActionFragment implements OnPreferenceChangeListener {
 
@@ -67,7 +67,7 @@ public class Buttons extends ActionFragment implements OnPreferenceChangeListene
     public static final int KEY_MASK_VOLUME = 0x40;
 
     private ListPreference mBacklightTimeout;
-    private CustomSeekBarPreference mButtonBrightness;
+    private SystemSettingSeekBarPreference mButtonBrightness;
     private SwitchPreference mButtonBrightness_sw;
     private SwitchPreference mHwKeyDisable;
 
@@ -99,7 +99,7 @@ public class Buttons extends ActionFragment implements OnPreferenceChangeListene
                     (ListPreference) findPreference(KEY_BACKLIGHT_TIMEOUT);
 
             mButtonBrightness =
-                    (CustomSeekBarPreference) findPreference(KEY_BUTTON_BRIGHTNESS);
+                    (SystemSettingSeekBarPreference) findPreference(KEY_BUTTON_BRIGHTNESS);
 
             mButtonBrightness_sw =
                     (SwitchPreference) findPreference(KEY_BUTTON_BRIGHTNESS_SW);

src/com/havoc/settings/fragments/IME.java
@@ -37,7 +37,7 @@ import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen; 
  
 import com.android.settings.SettingsPreferenceFragment; 
-import com.havoc.settings.preferences.CustomSeekBarPreference; 
+import com.havoc.settings.preferences.SystemSettingSeekBarPreference; 
 import com.android.settings.R; 
 import android.provider.Settings; 
 import android.view.Gravity; 

ArsenicRom/android_packages_apps_ArsenicSettings

src/com/arsenic/settings/fragments/Buttons.java
@@ -33,7 +33,7 @@ import com.android.settings.R;
 
 import com.android.settings.SettingsPreferenceFragment;
 
-import com.arsenic.settings.preferences.CustomSeekBarPreference;
+import com.arsenic.settings.preferences.SystemSettingSeekBarPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
 
@@ -49,7 +49,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private static final String CATEGORY_HWKEY = "hardware_keys";
 
     private ListPreference mBacklightTimeout;
-    private CustomSeekBarPreference mButtonBrightness;
+    private SystemSettingSeekBarPreference mButtonBrightness;
     private SwitchPreference mButtonBrightness_sw;
 
     @Override
@@ -65,7 +65,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         mBacklightTimeout =
                 (ListPreference) findPreference(KEY_BACKLIGHT_TIMEOUT);
         mButtonBrightness =
-                (CustomSeekBarPreference) findPreference(KEY_BUTTON_BRIGHTNESS);
+                (SystemSettingSeekBarPreference) findPreference(KEY_BUTTON_BRIGHTNESS);
         mButtonBrightness_sw =
                 (SwitchPreference) findPreference(KEY_BUTTON_BRIGHTNESS_SW);
 

src/com/arsenic/settings/fragments/IME.java
@@ -36,8 +36,8 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory; 
 import android.support.v7.preference.PreferenceScreen; 
  
-import com.android.settings.SettingsPreferenceFragment; 
-import com.arsenic.settings.preferences.CustomSeekBarPreference; 
+import com.android.settings.SettingsPreferenceFragment;
+import com.arsenic.settings.preferences.SystemSettingSeekBarPreference; 
 import com.android.settings.R; 
 import android.provider.Settings; 
 import android.view.Gravity; 